0aada76f19d30270c2b136ef22befdd6cdbf662b
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-11-10 16:57:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Your code here
+
+function saturdayFun (activity = "roller-skate") {
+    return (`This Saturday, I want to ${activity}!`);
+};
+
+mondayWork()
+function mondayWork(activity = "go to the office") {
+    return (`This Monday, I will ${activity}.`)
+};
+
+function wrapAdjective(hm = "*"){
+return function(ugh = "special") {
+return `You are ${hm+ugh+hm}!`
+}
+};
+
+const Calculator = {
+    add: function(){
+        return 1 + 3
+    },
+
+    subtract: function() {
+        return 1 - 3
+    },
+
+    multiply: function() {
+        return 1 * 3
+    },
+
+    divide: function() {
+        return 10/5
+    }
+}
+
+function actionApplyer(start, array) {
+    if(array.length === 0) {
+        return start
+    } else {
+        let answer = start
+        for (let action of array) {
+            answer = action(answer)
+        }
+        return answer
+    }
+}
